87ec9227dff1c6eb0da1d28ea3648671d3e8cdca
adrimarieg <adriannegonzalez@aol.com>
2025-03-28 16:52:41 -0400

    add extra practice folder, with extra exercises

---
+++ extra-practice/sum-of-numbers.cpp
@@ -0,0 +1,41 @@
+/*
+SUM OF NUMBERS
+
+Write a program that asks the user for a positive integer value. The program should use a loop to get the sum of 
+all the integers from 1 up to the nuber entered. For example, if the user enters 50, the loop will find the sum 
+of 1,2,3,4,...50.
+
+Input Validation: Do not accept a negative starting number. 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    char playAgain;
+    
+    do {
+        int input;
+        int sum = 0;
+        
+        cout << "Please enter a positive integer: ";
+        cin >> input;
+
+        if (input <= 0) {
+            cout << "Please enter a positive integer." << endl;
+            break;
+        } else {
+            for (int i = 0; i <= input; i++) {
+                sum += i;
+            }
+        }
+ 
+        cout << "The total sum of numbers leading up to your input value is: " << sum << endl;
+        cout << "Would you like to enter another number? Y/N" ;
+        cin >> playAgain;
+    } while (playAgain == 'y' || playAgain == 'Y');
+    
+    return 0;
+}
